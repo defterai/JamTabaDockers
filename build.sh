03359c3b45c6cc02bebe164a99439b601eb7b63d
Defter <defter@bigmir.net>
2022-10-07 21:37:02 +0300

    Fix windows base and inherited docker files

--- build.sh
+++ build.sh
@@ -7,10 +7,12 @@ docker pull ubuntu:20.04
 docker build --force-rm -f Dockerfile.linux -t defterai/qt-linux:v5.15.2 .
 
 # MXE (M cross environment)
-docker build --force-rm -f Dockerfile.mxe -t defterai/qt-mxe:v5.15.2 .
+docker build --force-rm -f Dockerfile.mxe -t defterai/qt-mxe:v5.15.6 .
 
 # Windows 32bit statically linked
-docker build --force-rm -f Dockerfile.win32 -t defterai/qt-win32:v5.15.2 .
+docker build --force-rm -f Dockerfile.win32 -t defterai/qt-win32:v5.15.6 .
 
 # Windows 64bit statically linked
-docker build --force-rm -f Dockerfile.win64 -t defterai/qt-win64:v5.15.2 .
+docker build --force-rm -f Dockerfile.win64 -t defterai/qt-win64:v5.15.6 .
+
+read -p "Press enter to continue"
