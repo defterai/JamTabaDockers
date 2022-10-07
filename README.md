f9f3b0cbd1e2104547102e84f4e30a28ec728170
Defter <defter@bigmir.net>
2022-10-07 17:40:46 +0300

    Prepare for use separate containers which reuse Qt only support containers images to reduce container build time

--- README.md
+++ README.md
@@ -3,14 +3,14 @@ JamTaba compile environment for Docker with support C++ Qt 5.15.2 static builds
 
 * Linux
 ```
-docker build --force-rm -f Dockerfile.linux -t defterai/jam-taba-linux:v5.15.2 .
-docker run --rm -it -v $(pwd):/app defterai/jam-taba-linux:v5.15.2
+docker build --force-rm -f Dockerfile.linux -t defterai/qt-linux:v5.15.2 .
+docker run --rm -it -v $(pwd):/app defterai/qt-linux:v5.15.2
 ```
 
 * Windows 64bit Cross-compiled
 ```
-docker build --force-rm -f Dockerfile.win64 -t defterai/jam-taba-win64:v5.15.2 .
-docker run --rm -it -v $(pwd):/app defterai/jam-taba-win64:v5.15.2
+docker build --force-rm -f Dockerfile.win64 -t defterai/qt-win64:v5.15.2 .
+docker run --rm -it -v $(pwd):/app defterai/qt-win64:v5.15.2
 cd /app
 qmake
 make -j $(nproc)
--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ set -euxo pipefail
 docker pull ubuntu:20.04
 
 # Ubuntu statically linked
-docker build --force-rm -f Dockerfile.linux -t defterai/jam-taba-linux:v5.15.2 .
+docker build --force-rm -f Dockerfile.linux -t defterai/qt-linux:v5.15.2 .
 
 # MXE (M cross environment)
-docker build --force-rm -f Dockerfile.mxe -t defterai/jam-taba-mxe:v5.15.2 .
+docker build --force-rm -f Dockerfile.mxe -t defterai/qt-mxe:v5.15.2 .
 
 # Windows 32bit statically linked
-docker build --force-rm -f Dockerfile.win32 -t defterai/jam-taba-win32:v5.15.2 .
+docker build --force-rm -f Dockerfile.win32 -t defterai/qt-win32:v5.15.2 .
 
 # Windows 64bit statically linked
-docker build --force-rm -f Dockerfile.win64 -t defterai/jam-taba-win64:v5.15.2 .
+docker build --force-rm -f Dockerfile.win64 -t defterai/qt-win64:v5.15.2 .
